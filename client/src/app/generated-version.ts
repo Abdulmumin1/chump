c0c7d281816ef18869fcf7ba85050803eefb88fc
Abdulmumin Yaqeen <abdulmuminyqn@gmail.com>
2026-08-13 11:56:43 +0100

    chore(release): fix server health after session load

--- client/src/app/generated-version.ts
+++ client/src/app/generated-version.ts
@@ -1,2 +1,2 @@
-export const CHUMP_CLIENT_VERSION = "0.3.2";
-export const CHUMP_SERVER_VERSION = "0.1.21";
+export const CHUMP_CLIENT_VERSION = "0.3.3";
+export const CHUMP_SERVER_VERSION = "0.1.22";
